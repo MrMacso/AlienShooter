d0ea413c70e9f11bdb3574e4502d3b7ceeb9b59f
Csaba Maczo <maczocsaba@gmail.com>
2023-07-28 16:46:37 +0100

    Base scene and setup player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float _jumpEndTime;
+    [SerializeField] float _horizontalVelocity = 3;
+    [SerializeField] float _jumpVelocity = 5;
+    [SerializeField] float _jumpDuration = 0.5f;
+    [SerializeField] Sprite _jumpSprite;
+    public bool IsGrounded;
+    SpriteRenderer _spriteRenderer;
+    Sprite _defaultSprite;
+
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _defaultSprite = _spriteRenderer.sprite;
+    }
+
+    void OnDrawGizmos()
+    {
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        Vector2 origin = new Vector2(transform.position.x, transform.position.y - spriteRenderer.bounds.extents.y);
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(origin, origin + Vector2.down * 0.1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 origin = new Vector2(transform.position.x, transform.position.y - _spriteRenderer.bounds.extents.y);
+        var hit = Physics2D.Raycast(origin, Vector2.down, 0.1f);
+        if (hit.collider)
+        {
+            IsGrounded = true;
+            _spriteRenderer.sprite = _defaultSprite;
+        }
+        else
+        {
+            IsGrounded = false;
+            _spriteRenderer.sprite = _jumpSprite;
+        }
+        var horizontal = Input.GetAxis("Horizontal");
+        Debug.Log(horizontal);
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        var vertical = rb.velocity.y;
+
+        if (Input.GetButtonDown("Fire1") && IsGrounded)
+            _jumpEndTime = Time.time + _jumpDuration;
+
+        if (Input.GetButton("Fire1") && _jumpEndTime > Time.time)
+            vertical = _jumpVelocity;
+
+        horizontal *= _horizontalVelocity;
+        rb.velocity = new Vector2(horizontal, vertical);
+    }
+}
