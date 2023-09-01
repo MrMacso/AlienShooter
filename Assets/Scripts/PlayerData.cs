462e071370533369f52b4a6622d892d03b1702f4
Csaba Maczo <maczocsaba@gmail.com>
2023-09-01 13:13:27 +0100

    Revert "Merge branch 'development' into main"
    This reverts commit 5f002ed0a0c0b04c45e1ad006e644953ebc7aff7.

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+[Serializable]
+public class PlayerData
+{
+    public int Coins;
+    public int Health = 6;
+}
+
+[Serializable]
+public class GameData
+{ 
+    public List<PlayerData> PlayerDatas = new List<PlayerData>();
+
+    public string GameName;
+}
