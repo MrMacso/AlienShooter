82db41ecc64f0bea16cdb50b8ca36ad37962acfb
Csaba Maczo <maczocsaba@gmail.com>
2023-09-04 14:35:06 +0100

    created PoolManager to handle pooled objects

---
+++ Assets/Scripts/PoolManager.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class PoolManager : MonoBehaviour
+{
+    [SerializeField] BlasterShot _blasterShotPrefab;
+    ObjectPool<BlasterShot> _pool;
+
+    public static PoolManager Instance { get; private set; }
+
+    void Awake()
+    {
+        Instance= this;
+        _pool = new ObjectPool<BlasterShot>(AddNewBlasterShotToPool,
+    t => t.gameObject.SetActive(true),
+    t => t.gameObject.SetActive(false));
+    }
+
+    BlasterShot AddNewBlasterShotToPool()
+    {
+        var shot = Instantiate(_blasterShotPrefab);
+        shot.SetPool(_pool);
+        return shot;
+    }
+
+    public BlasterShot GetBlasterShot()
+    {
+       return _pool.Get();
+    }
+}
