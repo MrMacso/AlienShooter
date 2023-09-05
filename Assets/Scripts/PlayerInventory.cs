20a0f006b9c8c1f32db3e3e040b89220ddb92ee6
Csaba Maczo <maczocsaba@gmail.com>
2023-09-05 13:12:40 +0100

    added player inventory item switching

--- Assets/Scripts/PlayerInventory.cs
+++ Assets/Scripts/PlayerInventory.cs
@@ -9,24 +9,46 @@ public class PlayerInventory : MonoBehaviour
 
     public Transform ItemPoint;
     PlayerInput _playerInput;
-    private Key _equipedKey;
+    Key EquipedKey => _items.Count >= _currentItemIndex ? _items[_currentItemIndex] : null;
+    List<Key> _items = new List<Key>();
+    int _currentItemIndex;
 
     void Awake()
     {
         _playerInput= GetComponent<PlayerInput>();
         _playerInput.actions["Fire"].performed += UseEquipedItem;
+        _playerInput.actions["EquipNext"].performed += EquipNext;
+    }
+
+    void EquipNext(InputAction.CallbackContext obj)
+    {
+        _currentItemIndex++;
+        if (_currentItemIndex >= _items.Count)
+            _currentItemIndex = 0;
+
+        ToggleEquippedItem();
+    }
+
+    private void ToggleEquippedItem()
+    {
+        for (int i = 0; i < _items.Count; i++)
+        {
+            _items[i].gameObject.SetActive(i == _currentItemIndex);
+        }
     }
 
     void UseEquipedItem(InputAction.CallbackContext obj)
     {
-        if(_equipedKey)
-            _equipedKey.Use();
+        if(EquipedKey)
+            EquipedKey.Use();
     }
 
     public void Pickup(Key key)
     {
         key.transform.SetParent(ItemPoint);
         key.transform.localPosition = Vector3.zero;
-        _equipedKey = key;
+        _items.Add(key);
+        _currentItemIndex = _items.Count - 1;
+        ToggleEquippedItem();
     }
 }
