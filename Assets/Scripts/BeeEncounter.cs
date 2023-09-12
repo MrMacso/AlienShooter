2b6f1e38e674d64ca6dbee887d4fb4e7cc72e8b3
Csaba Maczo <72753261+MrMacso@users.noreply.github.com>
2023-09-12 11:43:30 +0100

    added player damage and some lightning control + fix bee platform prefab

--- Assets/Scripts/BeeEncounter.cs
+++ Assets/Scripts/BeeEncounter.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,9 +6,19 @@ using UnityEngine;
 public class BeeEncounter : MonoBehaviour
 {
     [SerializeField] List<Transform> _lightnings;
-    [SerializeField] float _delayBetweenLightning = 2.5f;
+    [SerializeField] float _delayBeforeDamage = 1.5f;
+    [SerializeField] float _lightningAnimationTime = 2f;
+    [SerializeField] float _delayBetweenLightning = 1f;
+    [SerializeField] float _lightningRadius = 1f;
+    [SerializeField] LayerMask _playerLayer;
 
-    private void OnEnable()
+    Collider2D[] _playerHitResult = new Collider2D[10];
+    void OnValidate()
+    {
+        if(_lightningAnimationTime <= _delayBeforeDamage)
+            _delayBeforeDamage= _lightningAnimationTime;
+    }
+    void OnEnable()
     {
         StartCoroutine(StartEncounter());
     }
@@ -24,8 +35,24 @@ public class BeeEncounter : MonoBehaviour
 
             int index = UnityEngine.Random.Range(0, _lightnings.Count);
             _lightnings[index].gameObject.SetActive(true);
-
+            yield return new WaitForSeconds(_delayBeforeDamage);
+            DamagePlayersInrange(_lightnings[index]);
+            yield return new WaitForSeconds(_lightningAnimationTime - _delayBeforeDamage);
+            _lightnings[index].gameObject.SetActive(false);
             yield return new WaitForSeconds(_delayBetweenLightning);
+        }
+    }
+
+    void DamagePlayersInrange(Transform lightning)
+    {
+        int hits = Physics2D.OverlapCircleNonAlloc(
+            lightning.position,
+            _lightningRadius,
+            _playerHitResult,
+            _playerLayer);
+        for (int i = 0; i < hits; i++)
+        {
+            _playerHitResult[i].GetComponent<Player>().TakeDamage(Vector3.zero);
         }
     }
 }
